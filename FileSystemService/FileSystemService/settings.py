4a9fa78e21481fede89fb8cc399031f92be84121
Владимир Бибин <devbibinva@gmail.com>
2018-05-13 13:06:31 +0300

    Hide secret key

--- FileSystemService/FileSystemService/settings.py
+++ FileSystemService/FileSystemService/settings.py
@@ -16,11 +16,15 @@ import json
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Get Credentials for application
+with open(BASE_DIR + '/../credentials.json') as f:
+    data = json.load(f)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jlw_mjm$nrt-p)oq&cw3!$f$!u-uoo*#qkdp^4oy$3d*)#h#b4'
+SECRET_KEY = data["FileSystemSecretKey"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
