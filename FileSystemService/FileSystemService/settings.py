76a26aad9c4c4d69b1b63f3f774366e50342c38e
Владимир Бибин <devbibinva@gmail.com>
2018-05-13 12:59:31 +0300

    Switch to postgre database

--- FileSystemService/FileSystemService/settings.py
+++ FileSystemService/FileSystemService/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'FileSystemService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tutordb',
+        'USER': 'admin',
+        'PASSWORD': '000',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
