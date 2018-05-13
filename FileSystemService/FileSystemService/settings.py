2784427499ddad1bcc6532cb61753ca8be164afd
Владимир Бибин <devbibinva@gmail.com>
2018-05-13 13:08:21 +0300

    File conductor application was created

--- FileSystemService/FileSystemService/settings.py
+++ FileSystemService/FileSystemService/settings.py
@@ -16,7 +16,7 @@ import json
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Get Credentials for application
+# Get credentials for FileSystemService
 with open(BASE_DIR + '/../credentials.json') as f:
     data = json.load(f)
 
---
+++ FileSystemService/file_conductor_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class FileConductorAppConfig(AppConfig):
+    name = 'file_conductor_app'
