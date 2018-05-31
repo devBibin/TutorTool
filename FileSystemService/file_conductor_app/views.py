232c21c36b81608f029ebf1145a3a8924743c0b4
Владимир Бибин <devbibinva@gmail.com>
2018-05-31 17:50:23 +0300

    Add types of requests

--- FileSystemService/file_conductor_app/views.py
+++ FileSystemService/file_conductor_app/views.py
@@ -21,52 +21,54 @@ def index(request):
 
 
 def create_repo(request):
-    # Get current user
-    user = request.user
-    
-    # Create filesystem
-    fs = FileSystem.objects.create(master = user)
-    
-    # Create service's directories
-    Directory.objects.create(file_system=fs, 
-        name="Temp", 
-        is_editable=False, 
-        can_be_deleted=False)
+    if (request.method == "POST"):
+        # Get current user
+        user = request.user
+        
+        # Create filesystem
+        fs = FileSystem.objects.create(master = user)
+        
+        # Create service's directories
+        Directory.objects.create(file_system=fs, 
+            name="Temp", 
+            is_editable=False, 
+            can_be_deleted=False)
 
-    return HttpResponse("Created for " + str(user.username))
+        return HttpResponse("Created for " + str(user.username))
 
 
 def get_folder(request, folder_id=None):
-    # Get current user
-    user = request.user
+    if (request.method == "GET"):
+        # Get current user
+        user = request.user
 
-    # Get all info about current directory in current filesystem
-    fs = FileSystem.objects.get(master= user)
-    dirs = Directory.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
-    files = File.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
-    tests = Test.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
-    questions = Question.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
-    
-    # Get parent directory
-    try:
-        master = Directory.objects.get(pk=folder_id)
-    except:
-        master = None
-    
-    # Return result
-    return render(request, "file_conductor_app/file_system.html", 
-        {"dirs" : dirs,
-         "files" : files,
-         "tests" : tests,
-         "questions": questions,
-         "master": master,
-         })
+        # Get all info about current directory in current filesystem
+        fs = FileSystem.objects.get(master= user)
+        dirs = Directory.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
+        files = File.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
+        tests = Test.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
+        questions = Question.objects.filter(file_system=fs, parent=folder_id, is_deleted=False)
+        
+        # Get parent directory
+        try:
+            master = Directory.objects.get(pk=folder_id)
+        except:
+            master = None
+        
+        # Return result
+        return render(request, "file_conductor_app/file_system.html", 
+            {"dirs" : dirs,
+             "files" : files,
+             "tests" : tests,
+             "questions": questions,
+             "master": master,
+             })
 
 
 def create_folder(request, parent_id=None):
-    # Get parent folder
-    parent, parent_str = define_parent(parent_id)
     if (request.method == "POST"):
+        # Get parent folder
+        parent, parent_str = define_parent(parent_id)
         # Get user
         user = request.user
         # Get filesystem of user
@@ -80,25 +82,27 @@ def create_folder(request, parent_id=None):
 
 
 def remove_folder(request, id):
-    # Get current user
-    user = request.user
+    if (request.method == "GET"):
+        # Get current user
+        user = request.user
 
-    # Get and set deleted flag
-    d = Directory.objects.get(pk=id)
-    d.is_deleted = True
-    d.save()
+        # Get and set deleted flag
+        d = Directory.objects.get(pk=id)
+        d.is_deleted = True
+        d.save()
 
-    # Get current folder
-    parent, parent_str = define_parent(d.parent_id)
-    
-    # Return result
-    return HttpResponseRedirect(FILE_SYSTEM_URL + "file-system/" + parent_str)
+        # Get current folder
+        parent, parent_str = define_parent(d.parent_id)
+        
+        # Return result
+        return HttpResponseRedirect(FILE_SYSTEM_URL + "file-system/" + parent_str)
 
 
 def upload_file(request, parent_id=None):
-    # Get parent directory
-    parent, parent_str = define_parent(parent_id)
-    if request.method == 'POST':
+    if (request.method == 'POST'):
+        # Get parent directory
+        parent, parent_str = define_parent(parent_id)
+        
         # Get user
         user = request.user
 
@@ -129,30 +133,32 @@ def upload_file(request, parent_id=None):
 
 
 def download_file(request, id):
-    f_obj = File.objects.get(pk=id)
-    file_path = f_obj.path + "/" + f_obj.name
-    print file_path
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
-            return response
-    raise Http404
+    if (request.method == "GET"):
+        f_obj = File.objects.get(pk=id)
+        file_path = f_obj.path + "/" + f_obj.name
+        print file_path
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as fh:
+                response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+                response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+                return response
+        raise Http404
 
 
 def remove_file(request, id):
-    # Get current user
-    user = request.user
+    if (request.method == "GET"):    
+        # Get current user
+        user = request.user
 
-    # Get and set deleted flag
-    f = File.objects.get(pk=id)
-    f.is_deleted = True
-    f.save()
+        # Get and set deleted flag
+        f = File.objects.get(pk=id)
+        f.is_deleted = True
+        f.save()
 
-    parent, parent_str = define_parent(f.parent_id)
-    
-    # Return result
-    return HttpResponseRedirect(FILE_SYSTEM_URL + "file-system/" + parent_str)
+        parent, parent_str = define_parent(f.parent_id)
+        
+        # Return result
+        return HttpResponseRedirect(FILE_SYSTEM_URL + "file-system/" + parent_str)
 
 
 def user_auth(request):
